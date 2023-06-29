7cbbc47f3431587ee6213f0c5f85d2e1f0ba2f82
Shinkarenko Anton <AVEINDOMINUSNOX@yandex.ru>
2023-06-29 19:54:08 +0500

    add dev

--- backend/app.js
+++ backend/app.js
@@ -1,9 +1,9 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
+const cors = require('cors');
 const userRouter = require('./routes/users');
 const cardRouter = require('./routes/cards');
-const cors = require('cors');
 
 const { PORT = 3000 } = process.env;
 const app = express();
