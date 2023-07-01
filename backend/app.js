125294db43f59ff9880a7c4a2f9f2ed133cb8059
Shinkarenko Anton <AVEINDOMINUSNOX@yandex.ru>
2023-07-01 14:12:02 +0500

    fix: cors

--- backend/app.js
+++ backend/app.js
@@ -5,13 +5,13 @@ const rateLimit = require('express-rate-limit');
 const helmet = require('helmet');
 
 const { errors } = require('celebrate');
-const cors = require('./middlewares/cors');
 const userRouter = require('./routes/users');
 const cardRouter = require('./routes/cards');
 const { login, createUser } = require('./controllers/users');
 const handlerError = require('./middlewares/handlerError');
 const { validationCreateUser, validationLoginUser } = require('./middlewares/validationJoiUser');
 const { requestLogger, errorLogger } = require('./middlewares/logger');
+const cors = require('./middlewares/cors');
 
 const NotFoundError = require('./errors/notFoundError');
 
@@ -21,7 +21,6 @@ const app = express();
 mongoose.connect('mongodb://127.0.0.1:27017/mestodb');
 
 app.use(express.json());
-app.use(cors());
 
 const limiter = rateLimit({
   windowMs: 15 * 60 * 1000,
@@ -34,6 +33,8 @@ app.use(requestLogger);
 
 app.use(limiter);
 app.use(helmet());
+
+app.use(cors);
 
 app.post('/signin', validationLoginUser, login);
 app.post('/signup', validationCreateUser, createUser);
