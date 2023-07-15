ba198c84c03f16852003552935a3b02187ff1a07
Shinkarenko Anton <AVEINDOMINUSNOX@yandex.ru>
2023-07-15 19:42:14 +0500

    fix https

--- frontend/src/utils/Api.js
+++ frontend/src/utils/Api.js
@@ -107,7 +107,7 @@ class Api {
 
 const api = new Api({
  /*   url: "http://localhost:3000",  */
-  url: "http://mesto.aveindominusnox.nomoreparties.sbs",
+  url: "https://mesto.aveindominusnox.nomoreparties.sbs",
   token: `Bearer ${localStorage.getItem("token")}`,
   headers: {
     Authorization: `Bearer ${localStorage.getItem("token")}`,
