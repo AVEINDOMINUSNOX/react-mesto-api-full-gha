2223045e47ebca77d43f3b6d9a6a98826cdac210
Shinkarenko Anton <AVEINDOMINUSNOX@yandex.ru>
2023-07-19 16:40:02 +0500

    fix readme ip-adress

--- frontend/src/utils/Auth.js
+++ frontend/src/utils/Auth.js
@@ -1,5 +1,5 @@
-export const BASE_URL = "http://localhost:3000";
-/* export const BASE_URL = "https://mesto.aveindominusnox.nomoreparties.sbs"; */
+/* export const BASE_URL = "http://localhost:3000"; */
+export const BASE_URL = "https://mesto.aveindominusnox.nomoreparties.sbs";
 
 export class Auth {
   handleError(err) {
